f26f92dc438b2a8b46d8bbd7b25930da60740e38
danseiyadan <danseiyadan@gmail.com>
2022-02-11 22:02:04 +0900

    Create login_logout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,4 @@
 class ApplicationController < ActionController::Base
+    include SessionsHelper
+    # これを使うことでsessionhelperの内容が全部のcontrollerで使えるはず。
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,9 @@ Rails.application.routes.draw do
   get '/about', to: 'pages#about'
 
   resources :users
+
+  get "/login", to: 'sessions#new'
+  get "/logout", to: 'sessions#destroy'
+  resource :sessions, only: :create
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
