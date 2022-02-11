c1bdf1da251d97f6f58680712966f54c578591d6
danseiyadan <85783552+danseiyadan@users.noreply.github.com>
2022-02-11 21:28:21 +0900

    Create User_CRUD (#2)

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
 
   get '/home', to: 'pages#home'
   get '/about', to: 'pages#about'
+
+  resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
