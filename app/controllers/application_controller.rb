9361de5e28e5abb5324acd34e3dfda27e7f3fd7f
danseiyadan <danseiyadan@gmail.com>
2022-02-11 22:35:59 +0900

    Correct login_logout for the 2nd time

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,4 @@
 class ApplicationController < ActionController::Base
-    include SessionsHelper
-    # これを使うことでsessionhelperの内容が全部のcontrollerで使えるはず。
+  include SessionsHelper
+  # これを使うことでsessionhelperの内容が全部のcontrollerで使えるはず。
 end
