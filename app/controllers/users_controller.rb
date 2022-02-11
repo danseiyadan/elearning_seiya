58ae777c78f7ddbc9c19c70df05daf1050b0e3e2
danseiyadan <danseiyadan@gmail.com>
2022-02-11 20:18:41 +0900

    Create User_CRUD

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,48 @@
+class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "Account successfully created!"
+      redirect_to root_url
+    else
+      render "new"
+    end
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:success] = "Your profile has been updated"
+      redirect_to @user
+    else
+      render "edit"
+    end
+  end
+
+  def index
+    # @users = User.all
+    @users = User.paginate(page: params[:page], per_page: 10)
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end  
+
+  def destroy
+    User.find(params[:id]).destroy
+    redirect_to users_path
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+end
