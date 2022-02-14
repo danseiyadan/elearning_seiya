5d77ce05a4a8f5e34e25090b59c5cc8dc690d575
danseiyadan <danseiyadan@gmail.com>
2022-02-14 19:51:55 +0900

    Create category

--- config/routes.rb
+++ config/routes.rb
@@ -9,5 +9,7 @@ Rails.application.routes.draw do
   get "/login", to: 'sessions#new'
   get "/logout", to: 'sessions#destroy'
   resource :sessions, only: :create
+
+  resources :categories
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
